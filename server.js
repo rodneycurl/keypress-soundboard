6c20f9eb7ee394f88823d02258b694da62b4ec25
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-07-03 20:18:21 -0400

    updating soundboard

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(process.env.PORT || 5000, function(){
-    console.log('Server running on 8080...');
+    console.log('Server running on 5000...');
 });
